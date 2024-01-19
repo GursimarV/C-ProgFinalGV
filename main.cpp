88a23ba6557082ba638f54412dd177833ffaf88b
Gursimar Virdi <385491@colum.edu>
2024-01-18 22:24:19 -0600

    Add comments and change the code a bit

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
+#include <iomanip>
 
+//Enum to represent the two different transaction types
 enum TransactionType { Withdrawal, Deposit };
 
-class BankAccount {
+//The class to represent the bank account and its details
+class BankAccount{
 public:
     BankAccount() : AccountTotal(0) {}
     ~BankAccount() {}
@@ -17,27 +20,44 @@ public:
     int AccountTotal;
 };
 
-TransactionType convertToTransactionType(const std::string& type) {
-    if (type == "withdrawal") {
+//Converts the string of details to the two different transaction types
+TransactionType convertToTransactionType(const std::string& type)
+{
+    if (type == "withdrawal")
+    {
         return Withdrawal;
-    } else {
+    }
+    else
+    {
         return Deposit;
     }
 }
 
-void processTransaction(BankAccount& newAccount, std::vector<std::string>& accountStrings, std::vector<BankAccount>& bankAccounts) {
-    // Checks if string is in list already
+// Function to process a transaction and update account details
+void processTransaction(BankAccount& newAccount, std::vector<std::string>& accountStrings, std::vector<BankAccount>& bankAccounts)
+{
+    // Checks if string is in list of accounts
     auto it = std::find(accountStrings.begin(), accountStrings.end(), newAccount.AccountID);
-    if (it == accountStrings.end()) {
+    //If the account isn't in the list, add it up and update the balance
+    if (it == accountStrings.end())
+    {
         accountStrings.push_back(newAccount.AccountID);
         newAccount.AccountTotal += newAccount.trans_amt;
         bankAccounts.push_back(newAccount);
-    } else {
-        for (auto& account : bankAccounts) {
-            if (account.AccountID == newAccount.AccountID) {
-                if (newAccount.trans_type == Withdrawal) {
+    }
+    //If the account is in the list, find the account and update the balance
+    else
+    {
+        for (auto& account : bankAccounts)
+        {
+            if (account.AccountID == newAccount.AccountID)
+            {
+                if (newAccount.trans_type == Withdrawal)
+                {
                     account.AccountTotal -= newAccount.trans_amt;
-                } else {
+                }
+                else
+                {
                     account.AccountTotal += newAccount.trans_amt;
                 }
             }
@@ -45,10 +65,14 @@ void processTransaction(BankAccount& newAccount, std::vector<std::string>& accou
     }
 }
 
-void printAccountDetails(const std::vector<BankAccount>& bankAccounts) {
+//This is the function to print out the account details
+void printAccountDetails(const std::vector<BankAccount>& bankAccounts)
+{
     int accountNumber = 1;
-    for (const auto& account : bankAccounts) {
-        std::cout << "Account #" << accountNumber << ": " << account.AccountID << " $" << account.AccountTotal << std::endl;
+    for (const auto& account : bankAccounts)
+    {
+        //Print out the Account #, ID, and the total # of $
+        std::cout << "Account #" << accountNumber << ": " << account.AccountID << " $" << std::fixed << std::setprecision(2) << account.AccountTotal << std::endl;
         accountNumber++;
     }
 }
@@ -62,14 +86,17 @@ int main() {
     file_.open("output.txt");
 
     // Checks if the file is open
-    if (!file_.is_open()) {
+    if (!file_.is_open())
+    {
         std::cout << "Unable to open the file." << '\n';
         return 1;
     }
 
     BankAccount fooAcc;
 
-    while (file_ >> fooAcc.AccountID >> fooAcc.trans_amt) {
+    //Read the account details and the transactions in the file and processes
+    while (file_ >> fooAcc.AccountID >> fooAcc.trans_amt)
+    {
         std::string transTypeStr;
         file_ >> transTypeStr;
         fooAcc.trans_type = convertToTransactionType(transTypeStr);
@@ -77,6 +104,7 @@ int main() {
         processTransaction(fooAcc, accountStrings, bankAccounts);
     }
 
+    //Print out all 10 account details
     printAccountDetails(bankAccounts);
 
     file_.close();
